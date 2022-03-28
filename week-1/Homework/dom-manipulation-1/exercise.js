b0856b54431f4e9f043075668ed0fae6ae5b9dd2
jorgelinad <donquisjorgelina92@gmail.com>
2022-03-28 13:03:41 +0200

    homework

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -1,20 +1,30 @@
-/*
-Task 1
-=======
-Write JavaScript below that logs:
-    1. all the "p" element nodes of the document,
-    --> should log a list of nodes with a length of 6
 
-    2. the first div element node
-    --> should log the ".site-header" node
+//Task 1
+//=======
+//Write JavaScript below that logs:
+   // 1. all the "p" element nodes of the document,
+   //--> should log a list of nodes with a length of 6
 
-    3. the element with id "jumbotron-text"
-    --> should log the "#jumbotron-text" node
+   const paragraph = document.querySelectorAll("p")
+   console.log(paragraph);
 
-    4. all the "p" elements of contained inside  the .primary-content element node
-    --> should log a list of nodes with a length of 3
+    //2. the first div element node
+    //--> should log the ".site-header" node
 
-*/
+    const firstDiv = document.querySelector("div")
+    console.log(firstDiv);
+
+    //3. the element with id "jumbotron-text"
+    //--> should log the "#jumbotron-text" node
+
+    const jumbotronText = document.querySelector("#jumbotron-text")
+    console.log(jumbotronText)
+
+    //4. all the "p" elements of contained inside  the .primary-content element node
+    //--> should log a list of nodes with a length of 3
+    
+    const primaryContent = document.querySelector(".primary-content").querySelectorAll("p")
+    console.log(primaryContent);
 
 
 /*
@@ -23,7 +33,12 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+    const alertButton = document.querySelector("#alertBtn")
+    alertButton.addEventListener("click", alertBikes)
 
+    function alertBikes(){
+        alert("Thanks for visiting Bikes for Refugees!")
+    }
 
 /*
 Task 3
@@ -32,6 +47,14 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
+    const changeColour = document.querySelector("#bgrChangeBtn")
+    changeColour.addEventListener("click", styleBackground)
+
+    function styleBackground(){
+        const changeColour = document.querySelector("body")
+        const colour = ["pink", "blue", "red", "yelow", "green"]
+        changeColour.style.backgroundColor = "pink"
+    }
 
 /*
 Task 4
@@ -40,7 +63,14 @@ Task 4
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
 
+    const buttonAddSomeText = document.querySelector("#addTextBtn")
+    buttonAddSomeText.addEventListener("click", addSomeText)
 
+    function addSomeText(text){
+        const newParagraph = document.createElement("p")
+        mainArticles.appendChild(newParagraph)
+        newParagraph.innerText = "La bicicleta, coloquialmente llamada bici(también denominada como cicla o ciclo en Colombia, Cuba y Uruguay, chiva, en México y cleta o chancha en Chile), es un vehículo de transporte personal de propulsión humana. Es impulsada por el propio viajero, que acciona el vehículo con el esfuerzo muscular de las piernas, en particular mediante pedales o manivelas."
+    }
 
 /*
 Task 5
@@ -49,6 +79,13 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
+const largerLinks = document.querySelector("#largerLinksBtn")
+largerLinks.addEventListener("click", textIncrease)
+
+function textIncrease(){
+    const linksIncrease = document.querySelectorAll("a")
+    linksIncrease.forEach(a => a.style.fontSize = "1.5em")
+}
 
 /*
 Task 6
@@ -57,7 +94,16 @@ Task 6
 Using the same function in Task 4,
 When the 'Add' button is clicked, get the text inside the input field and create a new paragraph in the "LEARN MORE" section
 Also clear the text inside the input field
-*/
+
+
+const buttonAddSomeText = document.querySelector("#addTextBtn")
+    buttonAddSomeText.addEventListener("click", addSomeText)
+
+    function addSomeText(text){
+        const newParagraph = document.createElement("p")
+        mainArticles.appendChild(newParagraph)
+        newParagraph.innerText = "La bicicleta, coloquialmente llamada bici(también denominada como cicla o ciclo en Colombia, Cuba y Uruguay, chiva, en México y cleta o chancha en Chile), es un vehículo de transporte personal de propulsión humana. Es impulsada por el propio viajero, que acciona el vehículo con el esfuerzo muscular de las piernas, en particular mediante pedales o manivelas."
+    }
 
 /*
 Task 7
@@ -68,3 +114,4 @@ Using the same function in Task 3, every time the 'Change colour' button is clic
 The next color when you are in the last color of the array will be the first color again.
 */
 
+const colour = ["pink", "blue", "red", "yelow", "green"]
